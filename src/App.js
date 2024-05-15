2cf93970a581a28b0a7430f5ee22b73053cad426
PRASHANTH AOUSULA <prashanthchary91595@gmail.com>
2024-05-15 16:46:52 +0530

    its comment

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header/Header";
+import Home from "./components/Header/pages/Home";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Home />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div
+        style={{
+          border: "solid black 3px",
+          width: "100%",
+          height: "50px",
+          textAlign: "center",
+          backgroundColor: "yellow",
+        }}
+      >
+        I am Header
+      </div>
+    </div>
+  );
+};
+
+export default Header;
